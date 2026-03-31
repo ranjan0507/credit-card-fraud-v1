1b847fdbd033c939bbcec8ad076bafeb57bde627
ranjan0507 <ranjanvh05@gmail.com>
2026-03-31 22:02:01 +0530

    basic pipeline complete

--- src/data_features/preprocessing.py
+++ src/data_features/preprocessing.py
@@ -2,6 +2,12 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 def split_data(df):
-    return train_test_split(
-        df, df['Class'], test_size=0.2, stratify=df['Class'], random_state=42
+    X=df.drop(columns=['Class'])
+    y=df['Class']
+    X_temp , X_test , y_temp , y_test = train_test_split(
+        X, y, test_size=0.2, stratify=y, random_state=42
     )
+    X_train , X_val , y_train , y_val = train_test_split(
+        X_temp,y_temp,stratify=y_temp,random_state=42,test_size=0.25
+    )
+    return X_train,y_train,X_val,y_val,X_test,y_test
--- src/pipelines/training.py
+++ src/pipelines/training.py
@@ -1,22 +1,48 @@
-from src.data.data_loader import load_data
-from src.data.preprocessing import split_data
+from src.data_features.data_loader import load_data
+from src.data_features.preprocessing import split_data
 from src.pipelines.pipelines import get_pipeline
-from sklearn.metrics import classification_report , roc_auc_score
+from sklearn.metrics import classification_report , roc_auc_score , precision_recall_curve , auc , recall_score , precision_score , f1_score
 import joblib
-
+import numpy as np
 
 def run_training_pipeline(data_path,model_path):
 	df=load_data(data_path)
 
-	X_train,X_test,y_train,y_test = split_data(df)
+	X_train,y_train,X_val,y_val,X_test,y_test = split_data(df)
 
 	pipeline=get_pipeline()
 	pipeline.fit(X_train,y_train)
 
-	y_pred=pipeline.predict(X_test)
+	y_probs = pipeline.predict_proba(X_val)[:,1]
 
-	print(classification_report(y_test,y_pred))
-	print("ROC-AUC score : " , roc_auc_score(y_test,y_pred))
+	thresholds_prob = np.linspace(0,1,100)
 
-	joblib.dump(pipeline,f"{model_path}/model_pipeline.pkl")
+	best_t = None
+	best_f1=0
+	for t in thresholds_prob:
+		y_pred_t = (y_probs>=t).astype(int)
+		recall = recall_score(y_val,y_pred_t)
+		precision = precision_score(y_val,y_pred_t)
+		f1 = f1_score(y_val,y_pred_t)
+		
+		if f1>best_f1:
+			best_f1=f1
+			best_t=t
+
+		print(f"t : {t:.2f}  P={precision:.4f}  R={recall:.4f}  F1={f1:.4f}")
+
+	print(f"Best threshold : {best_t} , Best F1 : {best_f1}")
+
+	y_test_probs = pipeline.predict_proba(X_test)[:, 1]
+	y_test_pred = (y_test_probs >= best_t).astype(int)
+	print(classification_report(y_test,y_test_pred))
+
+	precision , recall , thresholds = precision_recall_curve(y_test,y_test_probs)
+	pr_auc = auc(recall,precision)
+	print("PR-AUC: ", pr_auc) 
+
+	print(f"ROC_AUC : ",roc_auc_score(y_test,y_test_probs))
+
+	joblib.dump({"model":pipeline,"threshold":best_t}
+		,f"{model_path}/model_pipeline.pkl")
 
